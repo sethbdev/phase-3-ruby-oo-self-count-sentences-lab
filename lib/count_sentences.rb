c5b0f6256e707b491055712205974d5bd3f418fc
Seth <seth.bbrendle@gmail.com>
2022-10-18 10:24:40 -0400

    commit tree 12e4000686c68b8513c44a90d4268381a8cb24d3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    #new_array = self.split("/[\.|\!|\?/]")
+    new_array = self.split("/\.+/" || "/\!+/" || "/\?+/")
+    new_array.size
   end
 end
